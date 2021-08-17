23f160e6a270a3eb64f5053ed1ebdd1e7c1eb316
Harrison Goeldner <74375558+harrisongoeldner@users.noreply.github.com>
2021-08-16 17:09:06 -0700

    Add files via upload

---
+++ import sys, os.py
@@ -0,0 +1,21 @@
+
+import sys, os
+from tkinter import Tk
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+from tkinter.filedialog import askopenfilename
+imagename = askopenfilename('.jpg)')
+
+imagename = "/Users/harrisongoeldner/OneDrive/Pictures/Olympus/2020/2020-02-15/P2130004.JPG"
+
+image = Image.open(imagename)
+
+exifdata = image.getexif()
+
+for tagid in exifdata:
+    tagname = TAGS.get(tagid, tagid)
+    data = exifdata.get(tagid)
+ #   if isinstance(data, bytes):
+  #      data = data.decode()
+    print(f"{tagname:25}: {data}")
